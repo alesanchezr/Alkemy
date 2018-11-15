5d217bfe96cfb24e30d311b253fb4eb6cde80fcc
jferragut <31458614+jferragut@users.noreply.github.com>
2018-11-15 11:02:18 -0500

    Finished VideoCarousel Component

--- src/components/videoCarousel.jsx
+++ src/components/videoCarousel.jsx
@@ -1,63 +1,43 @@
 import React from 'react'
-import {
-  CSSTransition,
-  TransitionGroup,
-} from 'react-transition-group';
-import ReactDOM from 'react-dom';
-import '../assets/video/Home_Work/Home_work.mp4';
-
+import PropTypes from 'prop-types'
 
 export default class VideoCarousel extends React.Component{
   constructor(props){
     super(props);
 
-    this.slides = [
-      {
-        img: '../assets/video/Home_Work/Home_work.jpg',
-        mp4: '../assets/video/Home_Work/Home_work.mp4',
-        webm: '../assets/video/Home_Work/Home_work.webm',
-        ogv: '../assets/video/Home_Work/Home_work.ogv'
-      },
-      {
-        img: '/Aloha-Mundo/Aloha-Mundo.jpg',
-        mp4: '../assets/video/Aloha-Mundo/Aloha-Mundo.mp4',
-        webm: '../assets/video/Aloha-Mundo/Aloha-Mundo.webm',
-        ogv: '../assets/video/Aloha-Mundo/Aloha-Mundo.ogv'
-      },
-      {
-        img: '../assets/video/Office-Day/Office-Day.jpg',
-        mp4: '../assets/video/Office-Day/Office-Day.mp4',
-        webm: '../assets/video/Office-Day/Office-Day.webm',
-        ogv: ''
-      },
-      {
-        img: '../assets/video/Aloha-Mundo/Aloha-Mundo.jpg',
-        mp4: '../assets/video/Aloha-Mundo/Aloha-Mundo.mp4',
-        webm: '../assets/video/Aloha-Mundo/Aloha-Mundo.webm',
-        ogv: '../assets/video/Aloha-Mundo/Aloha-Mundo.ogv'
-      }
-    ];
-
     this.state = {
-      counter: 0
+      currentIndex: 0,
+      slides: this.props.slides
     };
 
+    this.handleEnded = this.handleEnded.bind(this);
     this.prevSlide = this.prevSlide.bind(this);
     this.nextSlide = this.nextSlide.bind(this);
   }
 
+  componentDidMount(){
+    this.video.addEventListener("ended", e => this.handleEnded);
+  }
+  componentDidUpdate(prevProps, prevState) {
+    this.video.play();
+  }
+
+  handleEnded(e) {
+    const nextIndex = this.state.currentIndex + 1 < this.state.slides.length ? this.state.currentIndex + 1 : 0
+    this.setState({ currentIndex: nextIndex });
+  }
 
   prevSlide(){
-    var prevSlide = this.state.counter - 1 < 0 ? this.props.slides.length - 1 : this.state.counter - 1;
+    var prevSlide = this.state.currentIndex - 1 < 0 ? this.state.slides.length - 1 : this.state.currentIndex - 1;
     this.setState({
-      counter: prevSlide
+      currentIndex: prevSlide
     })
   }
 
   nextSlide(){
-    var nextSlide = this.state.counter + 1 < this.props.slides.length ? this.state.counter + 1 : 0;
+    var nextSlide = this.state.currentIndex + 1 < this.state.slides.length ? this.state.currentIndex + 1 : 0;
     this.setState({
-      counter: nextSlide
+      currentIndex: nextSlide
     })
   }
 
@@ -71,53 +51,25 @@ export default class VideoCarousel extends React.Component{
   }
 
   render(){
-
     return(
-      <div className="carousel" style={{height:'100vh',width:'100vw'}} >
+      <div className="vc-wrap" style={{height:'100vh',width:'100vw',overflow:'hidden'}} >
         {this.showIndicators}
-        <TransitionGroup>
-          <CSSTransition
-            timeout={500}
-            classNames="fade"
-            >
-            <video
-              autoPlay={true}
-              muted={true}
-              loop={true}
-              className="overflow-hidden"
-              style={{objectFit:'cover',width:'100%',height:'100%'}}
-              key={this.state.counter}
-              >
-              {
-                (this.slides[this.state.counter].mp4!=='')
-                ? (
-                    <source
-                      type="video/mp4"
-                      src={this.slides[this.state.counter].mp4}
-                    />
-                  )
-                :''
-                (this.slides[this.state.counter].webm!=='')
-                ? (
-                    <source
-                      type="video/webm"
-                      src={this.slides[this.state.counter].webm}
-                    />
-                  )
-                :''
-                (this.slides[this.state.counter].ogv!=='')
-                ? (
-                    <source
-                      type="video/ogg"
-                      src={this.props.slides[this.state.counter].ogv}
-                    />
-                  )
-                :''
-              }
-            </video>
-          </CSSTransition>
-        </TransitionGroup>
+        <video
+          autoPlay="true"
+          muted="true"
+          playsInline="true"
+          crossOrigin="true"
+          onEnded={this.handleEnded}
+          src={this.state.slides[this.state.currentIndex].mp4}
+          style={{objectFit:'cover',width:'100%',height:'100%',overflow:'hidden'}}
+          poster={this.state.slides[this.state.currentIndex].img}
+          ref={el => {this.video = el}}
+          />
       </div>
     )
   }
+}
+
+VideoCarousel.propTypes = {
+  slides: PropTypes.array
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,45 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Image from '../components/image'
+
 import VideoCarousel from '../components/videoCarousel'
+
+// Import video assets
+import video_1_jpg from '../assets/video/Home_Work/Home_work.jpg'
+import video_1_mp4 from '../assets/video/Home_Work/Home_work.mp4'
+
+import video_2_jpg from '../assets/video/Aloha-Mundo/Aloha-Mundo.jpg'
+import video_2_mp4 from '../assets/video/Aloha-Mundo/Aloha-Mundo.mp4'
+
+import video_3_jpg from '../assets/video/Office-Day/Office-Day.jpg'
+import video_3_mp4 from '../assets/video/Office-Day/Office-Day.mp4'
+
+import video_4_jpg from '../assets/video/Into_the_Wild/Into_the_Wild.jpg'
+import video_4_mp4 from '../assets/video/Into_the_Wild/Into_the_Wild.mp4'
+
+/* Define Slide Array
+Each object(slide) in array should have 2 keys:
+    1. img - image for the video fallback poster
+    2. mp4 - mp4 video file to use
+*/
+const slideArray = [
+  {
+    img: video_1_jpg,
+    mp4: video_1_mp4,
+  },
+  {
+    img: video_2_jpg,
+    mp4: video_2_mp4,
+  },
+  {
+    img: video_3_jpg,
+    mp4: video_3_mp4,
+  },
+  {
+    img: video_4_jpg,
+    mp4: video_4_mp4,
+  }
+];
 
 /*
 Layout props:
@@ -19,11 +57,11 @@ const HomePage = () => (
     headerTitle={[false,""]}>
 
     {/* Section 1 - Hero */}
-    <section id="hero"  className="homeHero">
+    <section className="homeHero">
     	<div  className="container-fluid px-0 position-relative">
 
         {/* Cover Video Slider */}
-        <VideoCarousel/>
+        <VideoCarousel slides={slideArray} showIndicators={false}/>
 
     		{/* Cover Text */}
 
