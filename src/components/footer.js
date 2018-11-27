d55839175a9107c876667c13d72ded4e10a3b86a
jferragut <31458614+jferragut@users.noreply.github.com>
2018-11-27 15:44:58 -0500

    added phone number, background, css, and copywrite links

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,7 +10,7 @@ _menuArray object details:
   submenu: a nested array for dropdown menu items
 */
 
-var _menuArray = [
+const _menuArray = [
   {
     name: 'Our Solutions',
     id: '1',
@@ -48,11 +48,23 @@ var _menuArray = [
   }
 ]
 
+const _subLinks = [
+  {name: 'Privacy Policy', id: '1', url: '/privacy-policy'},
+  {name: 'Terms & Conditions', id: '2', url: '/terms-and-conditions'},
+  {name: 'Site Map', id: '3', url: '/site-map'}
+]
+
 const Footer = (props) => (
     <footer className="footer wow fadeInUp py-5">
       <div className="container">
         <Row>
           {renderLinkAreas(_menuArray)}
+        </Row>
+        <Row>
+          <div className="copyText text-center position-absolute my-0">
+            Copyright &copy; 2018, Alkemy, Inc.&nbsp;&mdash;&nbsp;
+            {renderSubLinks(_subLinks)}
+          </div>
         </Row>
       </div>
     </footer>
@@ -64,16 +76,31 @@ const renderLinkAreas = (menu) => {
       <Col xs={12} md={6} lg={3} key={item.id}>
         <h4>{item.name}</h4>
         <ul className="list-unstyled ml-0">
-        {(item.links.map(link=>{return(<li key={link.id}><Link to={link.url}>{link.name}</Link></li>)}))}
-        {(item.phone)
-          &&(item.phone.map(phone=>{
-            return(
-              <li key={phone.id}>{phone.name}: <Link to={phone.url}>{phone.textNumber}</Link></li>
-            )}))}
-
+          {(item.links.map(link=>{return(<li key={link.id}><Link to={link.url}>{link.name}</Link></li>)}))}
+          {(item.phone)
+            &&(item.phone.map(phone=>{
+              return(
+                <li key={phone.id}>{phone.name}: <br/><Link to={phone.url}><strong>{phone.textNumber}</strong></Link><p>&emsp;&emsp;({phone.number})</p></li>
+              )
+            })
+          )}
         </ul>
       </Col>
     )
+  })
+}
+
+const renderSubLinks = (menu) => {
+  return menu.map((item,index)=>{
+    if(index!=menu.length-1){
+      return(
+        <span key={item.id}><Link to={item.url}>{item.name}&nbsp;|&nbsp;</Link></span>
+      )
+    }else{
+      return(
+        <span key={item.id}><Link to={item.url}>{item.name}</Link></span>
+      )
+    }
   })
 }
 
