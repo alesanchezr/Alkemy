790e50b9634def58b02669b120e5f2373725e67a
jferragut <31458614+jferragut@users.noreply.github.com>
2019-03-28 19:35:37 -0400

    finished logic for stepper

--- src/components/BuildYourDream.jsx
+++ src/components/BuildYourDream.jsx
@@ -3,75 +3,129 @@ import { Button, Col, Row, Form,
   FormGroup, Label, Input } from 'reactstrap'
 import Stepper from 'react-stepper-horizontal';
 
-const BuildYourDream = () => {
-  return(
-    <section className="dreamForm py-5 px-3">
-      <h1 className="text-center">Let Us Build Your Dream</h1>
-      <div className="container">
-        <p>Didn’t find what you need in our above plans? No problem! We can prepare a custom estimate based on your specific needs. Just fill out the fields below so that we can get a better picture of what kind of site you will need and we’ll do the rest. As soon as we have had a chance to review your information, you’ll get a follow-up call from one of our team members to discuss your project further and iron out all of the details.</p>
-        <Form
-          onSubmit={(e)=>handleClick(e)}
-          className="py-3 mb-0"
-          >
-          <Stepper steps={stepperSteps} activeStep={ formStep } />
-          {renderForm}
-          <FormGroup className="text-center">
-            <Button className="btn btn-primary" type="submit" value="submit">{formStep<stepperSteps.length?'Next Step':'Submit My Request'}</Button>
-          </FormGroup>
-          <Input className='hp' autoComplete="off" type="text" name="name" id="name" placeholder="Please tell us your Name" />
-        </Form>
-      </div>
-    </section>
-  )
-}
+export default class BuildYourDream extends React.Component {
+  constructor(){
+    super();
 
-const dreamForm = React.createRef();
-
-let formStep = 1;
-const stepperSteps = [
-  {title: 'Step One'},
-  {title: 'Step Two'},
-  {title: 'Step Three'},
-  {title: 'Step Four'}
-]
-
-const handleClick = (e)=>{
-  e.preventDefault()
-
-  if(formStep!==stepperSteps.length){
-    formStep++
-    console.log('step: ',formStep)
-  }else{
-    alert("we're done")
-    //send form
+    this.state={
+      formStep: 1,
+      stepperSteps: [
+        {
+          title: 'Step One',
+          onClick: (e)=>this.state.formStep[e.currentTarget.innerHTML]
+        },
+        {
+          title: 'Step Two',
+          onClick: (e)=>this.state.formStep[e.currentTarget.innerHTML]
+        },
+        {
+          title: 'Step Three',
+          onClick: (e)=>this.state.formStep[e.currentTarget.innerHTML]
+        },
+        {
+          title: 'Step Four',
+          onClick: (e)=>this.state.formStep[e.currentTarget.innerHTML]
+        }
+      ]
+    }
+    const formArea = React.createRef();
   }
-  renderForm()
-}
 
-const renderForm = ()=>{
-    if(formStep===1){
-        return(
+
+
+  handleNextClick = ()=>{
+    if(this.state.formStep!==this.state.stepperSteps.length){
+      this.setState({
+        formstep: this.state.formStep++
+      })
+      console.log(this.state.formStep)
+    }
+  }
+
+  handleSubmit = (e)=>{
+    e.preventDefault()
+  }
+
+  renderForm = ()=>{
+    switch(this.state.formStep){
+      case 1:
+        return (
           <Row form className="my-2 py-0">
-            <Col xs={12} md={4} className="my-2 py-0">
-              <FormGroup>
-                <Input type="text" name="fullName" id="fullName" placeholder="Please tell us your Full Name" />
-              </FormGroup>
-            </Col>
-            <Col xs={12} md={4} className="my-2 py-0">
-              <FormGroup>
-                <Input type="email" name="email" id="email" placeholder="Enter your Email Address" />
-              </FormGroup>
-            </Col>
-            <Col xs={12} md={4} className="my-2 py-0">
-              <FormGroup>
-                <Input type="tel" name="phone" id="phone" placeholder="Enter your Telephone Number" />
-              </FormGroup>
-            </Col>
+          <Col xs={12} md={4} className="my-2 py-0">
+          <FormGroup>
+          <Input type="text" name="fullName" id="fullName" placeholder="Please tell us your Full Name" />
+          </FormGroup>
+          </Col>
+          <Col xs={12} md={4} className="my-2 py-0">
+          <FormGroup>
+          <Input type="email" name="email" id="email" placeholder="Enter your Email Address" />
+          </FormGroup>
+          </Col>
+          <Col xs={12} md={4} className="my-2 py-0">
+          <FormGroup>
+          <Input type="tel" name="phone" id="phone" placeholder="Enter your Telephone Number" />
+          </FormGroup>
+          </Col>
           </Row>
         )
-    }else if(formStep===2){
-      return 'hello'
-    }
-}
+      case 2:
+        return (
+          <Row form className="my-2 py-0">
+          <Col xs={12} className="my-2 py-0">
+          <FormGroup>
+          <p>Do you presently have a website?</p>
+          <Input type="radio" name="hasWebsite" value="no" checked />
+          <Input type="radio" name="hasWebsite" value="yes" />
+          <Input type="url" name="websiteAddress" id="websiteAddress" placeholder="What is the address for your site? (www.yoursite.com)" />
+          </FormGroup>
+          </Col>
+          </Row>
+        )
+      case 3:
+        return (
+          <Row form className="my-2 py-0">
+          <Col xs={12} md={4} className="my-2 py-0">
+          <FormGroup>
+          <Input type="text" name="fullName" id="fullName" placeholder="Please tell us your Full Name" />
+          </FormGroup>
+          </Col>
+          <Col xs={12} md={4} className="my-2 py-0">
+          <FormGroup>
+          <Input type="email" name="email" id="email" placeholder="Enter your Email Address" />
+          </FormGroup>
+          </Col>
+          <Col xs={12} md={4} className="my-2 py-0">
+          <FormGroup>
+          <Input type="tel" name="phone" id="phone" placeholder="Enter your Telephone Number" />
+          </FormGroup>
+          </Col>
+          </Row>
+        )
+      }
+  }
 
-export default BuildYourDream;
+  render(){
+    return(
+      <section className="dreamForm py-5 px-3">
+        <h1 className="text-center">Let Us Build Your Dream</h1>
+        <div className="container">
+          <p>Didn’t find what you need in our above plans? No problem! We can prepare a custom estimate based on your specific needs. Just fill out the fields below so that we can get a better picture of what kind of site you will need and we’ll do the rest. As soon as we have had a chance to review your information, you’ll get a follow-up call from one of our team members to discuss your project further and iron out all of the details.</p>
+
+          <Form
+          onSubmit={(e)=>this.handleSubmit(e)}
+          className="py-3 mb-0"
+          >
+            <Stepper steps={this.state.stepperSteps} activeStep={ this.state.formStep } />
+
+            <div ref='formArea'>{this.renderForm(this.state.formStep)}</div>
+
+            <FormGroup className="text-center">
+              <Button className="btn btn-primary" type={this.state.formStep<this.state.stepperSteps.length?'button':'submit'} onClick={this.handleNextClick}>{this.state.formStep<this.state.stepperSteps.length?'Next Step':'Submit My Request'}</Button>
+            </FormGroup>
+            <Input className='hp' autoComplete="off" type="text" name="name" id="name" placeholder="Please tell us your Name" />
+          </Form>
+        </div>
+      </section>
+    )
+  }
+}
