7255c25d3468cd3511f3f13e0971e365a9ba91dc
jferragut <31458614+jferragut@users.noreply.github.com>
2019-04-25 10:19:06 -0400

    Clean up imports and add suspense to Web Development page

--- src/pages/web-development.js
+++ src/pages/web-development.js
@@ -1,16 +1,14 @@
-import React from 'react'
+import React, {Suspense} from 'react'
 import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import fluidImage from '../utils/utils.js'
-import { Card, CardImg, CardText, CardImgOverlay,
-  CardTitle,CardBody, CardFooter, CardDeck, Button,
-  Col, Row, Form, FormGroup, Label, Input } from 'reactstrap'
+import { Card, Button, Col, Row } from 'reactstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import Loading from '../components/loading.jsx'
 import Layout from '../components/layout'
 import ScrollWrapper from '../components/scrollWrapper.jsx'
-import BuildYourDream from '../components/BuildYourDream.jsx'
 
+const BuildYourDream = React.lazy(() => import('../components/BuildYourDream.jsx'));
 /*
 Layout props:
   pageTitle: SEO friendly title for the title bar
@@ -26,105 +24,107 @@ const WebDevelopment = ({data}) => {
 
   return(
     <ScrollWrapper onWindowScroll={handleScroll}>
-      <Layout
-        pageTitle="Custom Web Development | Alkemy, Inc."
-        renderHeaderSolid={true}
-        headerTitle={[false,""]}
-        bodyClasses="webDesign"
-        >
+      <Suspense fallback={<Loading size='2x'/>}>
+        <Layout
+          pageTitle="Custom Web Development | Alkemy, Inc."
+          renderHeaderSolid={true}
+          headerTitle={[false,""]}
+          bodyClasses="webDesign"
+          >
 
-        {/* Page Hero */}
-        <section className='pageHero'>
-        {data.webDevBanner.childImageSharp
-          && (
-          <Img
-            className="mx-0 px-0"
-            fluid={data.webDevBanner.childImageSharp.fluid}
-            alt="Professional Web Development Services to craft your dream app." />
-          )
-        }
-        </section>
+          {/* Page Hero */}
+          <section className='pageHero'>
+          {data.webDevBanner.childImageSharp
+            && (
+            <Img
+              className="mx-0 px-0"
+              fluid={data.webDevBanner.childImageSharp.fluid}
+              alt="Professional Web Development Services to craft your dream app." />
+            )
+          }
+          </section>
 
-        {/* Section 1 */}
-        <section className="px-5 mt-4 mb-5">
-          <Row>
-            <Col>
-              <h1 className="mb-4">{data.webDevelopmentJson.sections[0].blocks[0].heading}</h1>
-              <p className="mb-5">{data.webDevelopmentJson.sections[0].blocks[0].content}</p>
-              <p className="mb-5">{data.webDevelopmentJson.sections[0].blocks[1].content}</p>
-            </Col>
-          </Row>
-          <Row className="flex-column-reverse flex-md-row">
-            <Col xs={12} sm={6}>
-              <h2 className="mb-4">{data.webDevelopmentJson.sections[0].blocks[2].heading}</h2>
-              <p className="mb-4">{data.webDevelopmentJson.sections[0].blocks[2].content}</p>
-              <p className="mb-4">{data.webDevelopmentJson.sections[0].blocks[3].content}</p>
-              <Row>
-                <Col xs={12} sm={6}>
-                  <Button onClick={handleDiscussClick} className="btn btn-primary">Let's Discuss My Project</Button>
-                </Col>
-              </Row>
-            </Col>
-            <Col xs={12} sm={6} className='text-center'>
-            {data.webDevFlasks.childImageSharp
-              && (
-              <Img
-                imgStyle={{maxHeight:'auto',maxWidth:'500px',objectFit:'contain',margin:'0 auto'}}
-                fluid={data.webDevFlasks.childImageSharp.fluid}
-                alt="Alkemy knows the right languages and framworks to get the job done correctly." />
-              )
-            }
-            </Col>
-          </Row>
-        </section>
+          {/* Section 1 */}
+          <section className="px-5 mt-4 mb-5">
+            <Row>
+              <Col>
+                <h1 className="mb-4">{data.webDevelopmentJson.sections[0].blocks[0].heading}</h1>
+                <p className="mb-5">{data.webDevelopmentJson.sections[0].blocks[0].content}</p>
+                <p className="mb-5">{data.webDevelopmentJson.sections[0].blocks[1].content}</p>
+              </Col>
+            </Row>
+            <Row className="flex-column-reverse flex-md-row">
+              <Col xs={12} sm={6}>
+                <h2 className="mb-4">{data.webDevelopmentJson.sections[0].blocks[2].heading}</h2>
+                <p className="mb-4">{data.webDevelopmentJson.sections[0].blocks[2].content}</p>
+                <p className="mb-4">{data.webDevelopmentJson.sections[0].blocks[3].content}</p>
+                <Row>
+                  <Col xs={12} sm={6}>
+                    <Button onClick={handleDiscussClick} className="btn btn-primary">Let's Discuss My Project</Button>
+                  </Col>
+                </Row>
+              </Col>
+              <Col xs={12} sm={6} className='text-center'>
+              {data.webDevFlasks.childImageSharp
+                && (
+                <Img
+                  imgStyle={{maxHeight:'auto',maxWidth:'500px',objectFit:'contain',margin:'0 auto'}}
+                  fluid={data.webDevFlasks.childImageSharp.fluid}
+                  alt="Alkemy knows the right languages and framworks to get the job done correctly." />
+                )
+              }
+              </Col>
+            </Row>
+          </section>
 
-        {/* Section 2 */}
-        <section className="deliverYourMessage mb-4 py-4">
-          <h1>{data.webDevelopmentJson.sections[1].heading}</h1>
-          <Row className="px-5 pt-4">
-            <Col xs={12} md={6}>
-            {data.webDevGraphic.childImageSharp
-              && (
-              <Img
-                imgStyle={{maxHeight:'auto',maxWidth:'550px',objectFit:'contain'}}
-                className="my-auto"
-                fluid={data.webDevGraphic.childImageSharp.fluid}
-                alt="Alkemy builds and crafts your application to specification." />
-              )
-            }
-            </Col>
+          {/* Section 2 */}
+          <section className="deliverYourMessage mb-4 py-4">
+            <h1>{data.webDevelopmentJson.sections[1].heading}</h1>
+            <Row className="px-5 pt-4">
+              <Col xs={12} md={6}>
+              {data.webDevGraphic.childImageSharp
+                && (
+                <Img
+                  imgStyle={{maxHeight:'auto',maxWidth:'550px',objectFit:'contain'}}
+                  className="my-auto"
+                  fluid={data.webDevGraphic.childImageSharp.fluid}
+                  alt="Alkemy builds and crafts your application to specification." />
+                )
+              }
+              </Col>
 
-            <Col xs={12} md={6}>
-              <h2 className="mb-4">{data.webDevelopmentJson.sections[1].blocks[0].heading}</h2>
-              <p className="text-white">{data.webDevelopmentJson.sections[1].blocks[0].content}</p>
-              <Button onClick={handleDiscussClick} className="btn btn-primary">Let's Discuss My Project</Button>
-            </Col>
-          </Row>
-        </section>
+              <Col xs={12} md={6}>
+                <h2 className="mb-4">{data.webDevelopmentJson.sections[1].blocks[0].heading}</h2>
+                <p className="text-white">{data.webDevelopmentJson.sections[1].blocks[0].content}</p>
+                <Button onClick={handleDiscussClick} className="btn btn-primary">Let's Discuss My Project</Button>
+              </Col>
+            </Row>
+          </section>
 
-        {/* Section 3 */}
-        <section className="wordpressDesign mb-4 py-4">
-          <Row className="px-5 pt-4">
-            <Col xs={12} md={7}>
-              <h2 className="mb-4">{data.webDevelopmentJson.sections[2].blocks[0].heading}</h2>
-              <p className="">{data.webDevelopmentJson.sections[2].blocks[0].content}</p>
-            </Col>
+          {/* Section 3 */}
+          <section className="wordpressDesign mb-4 py-4">
+            <Row className="px-5 pt-4">
+              <Col xs={12} md={7}>
+                <h2 className="mb-4">{data.webDevelopmentJson.sections[2].blocks[0].heading}</h2>
+                <p className="">{data.webDevelopmentJson.sections[2].blocks[0].content}</p>
+              </Col>
 
-            <Col xs={12} md={5}>
-            {data.codeScreen.childImageSharp
-              && (
-              <Img
-                className="my-auto"
-                fluid={data.codeScreen.childImageSharp.fluid}
-                alt="Let us handle the coding, so that your project is built skillfully." />
-              )
-            }
-            </Col>
-          </Row>
-        </section>
+              <Col xs={12} md={5}>
+              {data.codeScreen.childImageSharp
+                && (
+                <Img
+                  className="my-auto"
+                  fluid={data.codeScreen.childImageSharp.fluid}
+                  alt="Let us handle the coding, so that your project is built skillfully." />
+                )
+              }
+              </Col>
+            </Row>
+          </section>
 
-        <section ref={dreamForm}><BuildYourDream /></section>
-      </Layout>
+          <section ref={dreamForm}><BuildYourDream /></section>
+        </Layout>
+      </Suspense>
     </ScrollWrapper>
   )
 }
