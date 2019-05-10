54ac3f6382823a5e66f84fa055a45465b1873c6f
jferragut <31458614+jferragut@users.noreply.github.com>
2019-05-09 21:06:08 -0400

    attempted fix for build hangup on netlify

--- gatsby-node.js
+++ gatsby-node.js
@@ -118,3 +118,9 @@ exports.createPages = ({ actions, graphql }) => {
     });
   });
 };
+
+const ChildProcess = require('child_process');
+
+exports.onPostBuild = () => {
+  ChildProcess.execSync("ps aux | grep jest | grep -v grep | awk '{print $2}' | xargs kill");
+};
