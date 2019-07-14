471425dcb2e0a6ee108c8b6b898a76e6721043aa
jferragut <31458614+jferragut@users.noreply.github.com>
2019-07-14 17:14:59 -0400

    added text to Web Design page #42

--- src/components/seo.js
+++ src/components/seo.js
@@ -88,14 +88,6 @@ function SEO({
                 content="304B53089DF131D38A8031F5232E9FB1"
             />
 
-            <link rel="preconnect" href="https://www.google-analytics.com" />
-            <link rel="preconnect" href="https://www.google.com" />
-            <link rel="preconnect" href="https://www.alkemyinc.com" />
-            <link
-                rel="preconnect"
-                href="https://marketingplatform.google.com"
-            />
-
             <meta name="description" content={metaDescription} />
             <meta name="keywords" content={pageKeywords} />
             <meta name="author" content={pageAuthor} />
@@ -190,6 +182,7 @@ function SEO({
 
             <script
                 type="text/javascript"
+                async
                 src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"
             ></script>
 
