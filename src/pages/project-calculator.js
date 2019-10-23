55da766abd45b073b34813f467d24dd1d4edb84c
jferragut <31458614+jferragut@users.noreply.github.com>
2019-10-23 00:39:47 -0400

    created estimation tool component #25

--- src/pages/project-calculator.js
+++ src/pages/project-calculator.js
@@ -1,23 +1,15 @@
-import React,{useEffect} from "react";
+import React from "react";
 import { graphql, Link } from "gatsby";
 import Img from "gatsby-image";
 import { fluidImageSmall } from "../utils/utils.js";
 import {
-    CardDeck,
-    Card,
-    CardText,
-    CardImgOverlay,
-    CardTitle,
-    CardBody,
-    CardFooter,
-    Button,
-    Col,
-    Row,
+    Button,Col,Row
 } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Layout from "../components/layout.js";
 import ScrollWrapper from "../components/scrollWrapper.jsx";
 import SEO from "../components/seo";
+import ProjectEstimationTool from "../components/ProjectEstimationTool";
 
 /*
 Layout props:
@@ -41,14 +33,45 @@ const ProjectCalculator = ({ data }) => {
 
     return (
         <ScrollWrapper onWindowScroll={handleScroll}>
-            <Layout headerTitle={[true, pageTitle]} bodyClasses="projectCalculator">
+            <Layout
+                renderHeaderSolid={true}
+                headerTitle={[true, pageTitle]}
+                bodyClasses="projectCalculator"
+            >
                 <SEO title={pageTitle.name} />
 
                 {/* Section 1 */}
-                <section className="">
-                    
+                <section className="intro alk-container my-4">
+                    <h2 className="mb-4">
+                        Welcome to our Project Estimation Tool!
+                    </h2>
+                    <p>
+                        We understand that sometimes, estimating a project
+                        budget can be tough. However, setting aside a budget is
+                        crucial to being successful with any development
+                        project. So to help you get a ballpark ready, we have
+                        decided to provide this convenient project estimation
+                        tool.
+                    </p>
+                    <p>
+                        To use the tool, just fill out the questions on the form
+                        and our system will estimate your project cost.
+                    </p>
                 </section>
-                
+
+                {/* Section 2 */}
+                <section className="main-calulator alk-container">
+                    <ProjectEstimationTool />
+                </section>
+
+                {/* Section 3 */}
+                <section className="disclaimer alk-container">
+                    <p className="small">
+                        These numbers shouldn&apos;t be taken as a final amount,
+                        but rather, should be used to get an idea of where your
+                        costs may be.
+                    </p>
+                </section>
             </Layout>
         </ScrollWrapper>
     );
